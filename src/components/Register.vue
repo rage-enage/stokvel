d556a2a733bdc0d42e2c8e6b2f717e1807dbc2a2
Gio <gio@incendiaryblue.com>
2017-06-10 15:47:25 +0200

    Commiting my work far

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -2,8 +2,18 @@
 
     <div class="register">
         <form v-on:submit.prevent>
-            <label>Name: {{meow}}</label>
-            <input type='text'/>
+            <label>First Name: {{meow}}</label>
+            <input type='text' v-model="register.name"/>
+            <br/>
+            <label>Last Name: </label>
+            <input type='text'v-model="register.surname"/>
+            <br/>
+            <label>Email address:</label>
+            <input type='text'v-model="register.email"/>
+            <br/>
+            <label>Password:</label>
+            <input type='password'v-model="register.password"/>
+            <br/>
             <input type='submit' v-on:click="register"/>
         </form>
     </div>
@@ -15,13 +25,22 @@ export default {
   name: 'register',
   data() {
       return {
-          meow: 'rawr'
+        register: {
+            name: '',
+            surname: '',
+            email: '',
+            password: ''
+        },
+        meow: 'rawr'
       };
   },
   methods: {
     register: function (event) {
         event.preventDefault();
+        console.log("hello", this.register)
+        console.log(JSON.stringify(this.register))
         this.$http.get('https://rehive.com/api/3/').then(response => {
+            console.log(JSON.stringify(this.register))
             console.log(response)
             this.meow = response.body;
             this.$swal({
